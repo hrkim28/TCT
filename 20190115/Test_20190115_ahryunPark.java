088491e177afbd4d2558f2b07fe5fe7d716d4153
ahryunPark <balen214@lgcns.com>
2019-01-15 14:58:57 +0900

    modify

---
+++ 20190115/Test_20190115_ahryunPark.java
@@ -0,0 +1,48 @@
+package com.lgcns.exercise.plmtct;
+
+import java.util.ArrayList;
+
+public class Test_20190115{
+	
+	public static void main(String[] args) {
+		int input = 100;
+		
+		ArrayList<Integer> result = new ArrayList<>();
+		
+		result.add(0);
+		result.add(1);
+		
+		if(input > 2) {
+			
+			for(int inx = 2 ; inx <input; inx++) {
+				int sum = 0;
+				sum = result.get(inx-2) + result.get(inx-1);
+				if(sum < input ) {
+					result.add(sum);
+				}else {
+					break;
+				}
+				
+			}
+			
+		}else {
+			System.out.println("Input값은 3이상이어야 합니다.");
+		}
+		
+		
+		//값 출력
+		for(int i=0; i< result.size(); i++){
+			if(i == 0 ) {
+				System.out.print(result.get(i));
+			}else {
+				System.out.print( " , "+ result.get(i));
+			}
+			
+		}
+
+	}
+
+	
+
+}
+
