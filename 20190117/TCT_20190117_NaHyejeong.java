b8e07bf27207a7cc7e0f9c491f147959358dd925
hjna1987 <ralra1108@naver.com>
2019-01-17 14:01:09 +0900

    나혜정

--- 20190117/TCT_20190117_NaHyejeong.java
+++ 20190117/TCT_20190117_NaHyejeong.java
@@ -15,34 +15,38 @@ public class TCT_20190117_NaHyejeong {
 		int quo = inputNum/3;
 		
 		ArrayList<int[]> set = new ArrayList<int[]>();
-		int cnt = 0;
 		
+		int[] array = new int[3];
+		
+		// 입력값 확인
 		if( quo >= 3 && inputNum%3 == 0) {
-			int[] array = new int[3];
+			
 			for (int i = 1; i < quo-1; i++) {
 				array[0] = i;
 				for (int j = 1; j < quo-i; j++) {
+					
 					array[1] = j;
 					array[2] = quo-i-j;
-					cnt++;
-					set.add(array);
-					//System.out.println(3*array[0] +"+"+3*array[1] +"+"+3*array[2]);
+					set.add(array); // 배열을 결과값 목록에 저장
+					
+					// 배열 초기화
+					array = new int[3];
+					array[0] = i;
 				}
 			}
 		}else {
 			System.out.println("잘못된 입력값입니다");
 		}
 		
+		System.out.println("");
 		//결과 출력 
-		
-		
-		for (int[] array : set) {
+		for (int[] a : set) {
 			System.out.print(inputNum + " = ");
-			System.out.print(3*array[0] +" + "+3*array[1] +" + "+3*array[2]);
+			System.out.print(3*a[0] +" + "+3*a[1] +" + "+3*a[2]);
 			System.out.print(" / ");
 		}
-		
-		System.out.print("총"+cnt+"개");
+		System.out.println("");
+		System.out.println("총"+set.size()+"개");
 	}
 
 }
