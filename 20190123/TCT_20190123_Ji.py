4c7e111f48fa230fb8c4cc473e7d62d8ce8d7e05
srjie <46413743+srjie@users.noreply.github.com>
2019-01-23 13:45:53 +0900

    Add files via upload

---
+++ 20190123/TCT_20190123_Ji.py
@@ -0,0 +1,20 @@
+
+num = int(input ("N = "))
+i = 0
+num_list = []
+while i < num:
+    num_list.append( int(input()))
+    i=i+1
+
+xor_list = []
+for n in num_list:
+    if xor_list.count(n) > 0:
+        xor_list.remove(n)
+    else:
+        xor_list.append(n)
+
+xor_result = xor_list[0]
+for x in xor_list:
+    xor_result = xor_result | x
+
+print (xor_result)
