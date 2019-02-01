2761211e61a41551957ab2c49c2ef8792c667adf
ahryunPark <balen214@lgcns.com>
2019-02-01 11:10:52 +0900

    박아련 Update

--- 20190129/TCT_20190129_ahryunPark.java
+++ 20190129/TCT_20190129_ahryunPark.java
@@ -49,18 +49,17 @@ public class TCT_20190129_ahryunPark {
 		TCT_20190129_ahryunPark tct = new TCT_20190129_ahryunPark();
 		
 		String input1 = "9+5-4+8";
-//		String input2 = "5+6*3-6/2+5";
-		String input2 = "5+6*3-6+5";
-		
-//		System.out.println(tct.getReuslt(input1));
+		String input2 = "5+6*3-6/2+5";
+		System.out.println("Case1");
+		System.out.println(tct.getReuslt(input1));
+		System.out.println("Case2");
 		System.out.println(tct.getReuslt(input2));
-    	
 		
 
 	}
 	
 	
-	public String getReuslt(String input){
+	private String getReuslt(String input){
 		
 		String msg = "";
 		
@@ -68,113 +67,89 @@ public class TCT_20190129_ahryunPark {
 		String sub = "-";
 		String div = "/";
 		String multi = "*";
-		int count = 1;
 		char[] charList = input.toCharArray();
-		int checkIndex = charList.length;
 		
-//		while(count != 0) {
-//			for(int inx = 0 ; inx < checkIndex; inx++) {
-//				if(String.valueOf(charList[inx]).equals(div) || String.valueOf(charList[inx]).equals(multi) ) {
-//					if(input.equals(div)) {
-//						
-//					}else {
-//						
-//					}
-//					
-//				}else if(String.valueOf(charList[inx]).equals(add) || String.valueOf(charList[inx]).equals(sub)) {
-//					if(String.valueOf(charList[inx]).equals(add)) {
-//						String result = "";
-//						int startN = 0;
-//						int ednN = 0;
-//						for(int jnx = inx+1; jnx < checkIndex; jnx++) {
-//							
-//							if(charList[jnx] != '#') {
-//								startN = Integer. charList[jnx];
-//								charList[jnx] = '#';
-//								break;
-//							}
-//						}
-//						
-//						for(int jnx = inx-1; jnx > 0; jnx--) {
-//							
-//							if(charList[jnx] != '#') {
-//								ednN = (int)charList[jnx];
-//								charList[jnx] = '#';
-//								break;
-//							}
-//						}
-//						int temp = startN + ednN;
-//						charList[inx] = (char)temp;
-//						
-//						for(int jnx = 0; jnx < checkIndex; jnx++) {
-//							
-//							if(charList[jnx] != '#') {
-//								result = result + charList[jnx];
-//							}
-//						}
-//						
-//						System.out.println(result);
-//						count--;
-//						
-//					}else {
-//						
-//					}
-//				}
-//			}
-//		}
-//		
+		ArrayList<String> wordList = new ArrayList<String>();
 		
+		for(int inx = 0 ; inx < charList.length; inx++) {
+			wordList.add(String.valueOf(charList[inx]));
+		}
 		
-		String[] tempList = new String[input.length()];
-		
-		for(int inx = 0; inx < input.length(); inx++) {
-			String check = input.substring(inx, inx+1);
-			if(check.equals(add) || check.equals(sub) || check.equals(div) || check.equals(multi)){
-			tempList[inx] = check;
-			count++;
+			
+		//곱셈 , 나눗셈 사칙연산
+		if(wordList.contains("*") || wordList.contains("/")) {
+			
+			for(int inx = 0; inx <  wordList.size(); inx++) {
+				String check = wordList.get(inx);
+				if(check.equals("*") || check.equals("/") ) {
+					
+					int result = getCal(check, Integer.valueOf(wordList.get(inx-1)), Integer.valueOf(wordList.get(inx+1)));
+					wordList.add(inx, String.valueOf(result));
+					wordList.remove(inx+1);
+					wordList.remove(inx+1);
+					wordList.remove(inx-1);
+					this.printOperation(wordList);
+					inx = 0;
+					
+				}
 			}
 		}
 		
-		
-		if(input.contains(div) || input.contains(multi)) {
+		//더하기, 빼기 사칙연산
+		if(wordList.contains("+") || wordList.contains("-")) {
 			
-			int divIndex = input.indexOf(div);
-			int multiIndex = input.indexOf(multi);
-			
-			if(divIndex < multiIndex && divIndex > 0 ) {
-				int startN = 0;
-				int endN = 0;
-				for(int inx = divIndex+1; inx < input.length(); inx++) {
-					if(input.equals(add) || input.equals(sub) || input.equals(div) || input.equals(multi) ) {
-						endN = Integer.valueOf(input.substring(divIndex+1, inx));
-					}
+			for(int inx = 0; inx <  wordList.size(); inx++) {
+				String check = wordList.get(inx);
+				if(check.equals("+") || check.equals("-") ) {
+					
+					int result = getCal(check, Integer.valueOf(wordList.get(inx-1)), Integer.valueOf(wordList.get(inx+1)));
+					wordList.add(inx, String.valueOf(result));
+					wordList.remove(inx+1);
+					wordList.remove(inx+1);
+					wordList.remove(inx-1);
+					this.printOperation(wordList);
+					inx = 0;
 				}
-				
-				for(int inx = divIndex-1; inx > 0; inx--) {
-					if(input.equals(add) || input.equals(sub) || input.equals(div) || input.equals(multi) ) {
-						startN = Integer.valueOf(input.substring(inx, divIndex-1));
-					}
-				}
-				
-				int result = startN * endN;
-				count--;
-				
-				
-				
-
-				
 			}
-			else if (multiIndex < divIndex && multiIndex > 0 ) {
-				
-			}
-			
-		}else {
-			
 		}
 		
 		
 		return msg;
 		
 	}
+	
+	//사칙연산 결과값 구하기
+	private int getCal(String operation, int preNum, int nextNum) {
+		int result = 0;
+		
+		switch(operation) {
+			case "*":
+				result = preNum * nextNum;
+				break;
+			case "/":
+				result = preNum / nextNum;
+				break;
+			case "+":
+				result = preNum + nextNum;
+				break;
+			case "-":
+				result = preNum - nextNum;	
+				break;
+		}
+		
+		return result;
+	}
+	
+	private void printOperation(ArrayList<String> wordList) {
+		
+		if(!wordList.isEmpty()) {
+			for(int inx = 0 ; inx < wordList.size(); inx++) {
+				System.out.print(wordList.get(inx));
+			}
+			System.out.println("");
+		}
+		
+	}
+	
 
 }
