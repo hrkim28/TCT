d9e1d9a23d10fbdba2c545e9da50ae8e3d257f4c
ckh80 <46431692+ckh80@users.noreply.github.com>
2019-02-12 18:32:58 +0900

    최기훈 commit

---
+++ 20190212/TCT_20190212_ChoiKiHun.py
@@ -0,0 +1,16 @@
+digit = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+result = ""
+val = input()
+222
+
+for i in val :
+	if digit.count(i) > 0 :
+		digit.remove(i)
+		result += i
+	else :
+		rear = val
+		
+
+# 추후에 작성해 보겠습니다.
+		
