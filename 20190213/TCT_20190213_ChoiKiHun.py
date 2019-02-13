dfddd54f4fc9a02f8ddfa7991e22f67df9a0cd6f
ckh80 <46431692+ckh80@users.noreply.github.com>
2019-02-13 14:03:03 +0900

    최기훈 commit

---
+++ 20190213/TCT_20190213_ChoiKiHun.py
@@ -0,0 +1,21 @@
+
+digit = 30
+count = 0
+
+while digit > 1 :
+	if digit % 3 != 0 :
+		if (digit - 1) % 3 == 0 :
+			digit = digit - 1
+			count = count + 1
+			digit = digit / 3
+			count = count + 1
+			continue
+			
+	if digit % 3 == 0 :
+		digit = digit / 3
+	elif digit % 2 == 0 :
+		digit = digit / 2
+		
+	count = count + 1
+		
+print(count)
