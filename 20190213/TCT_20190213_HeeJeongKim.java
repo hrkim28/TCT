7e16a6a422ae9d79ad09c790fa6ee891a72ea88a
hjkimsky <hjkimsky@lgcns.com>
2019-02-14 08:46:40 +0900

    김희정 update

--- 20190213/TCT_20190213_HeeJeongKim.java
+++ 20190213/TCT_20190213_HeeJeongKim.java
@@ -41,16 +41,11 @@ public class TCT_20190213_HeeJeongKim {
 
 		while (n>1) {
 			
-			int operone = (n%2==0)? n/2: (n%3==0)? n/3:-1;
-			int opertwo = ((n-1)%2==0)?(n-1)/2:((n-1)%3==0)?((n-1)/3):-1;
-
-			 if (operone==1 || operone<=opertwo ){ //|| opertwo==null) {
-		    	opertimes = opertimes + 1;
-		    	n=operone;
-		    } else if (operone > opertwo ) {//|| operone==null) {
-		    	opertimes = opertimes + 2;
-		    	n=opertwo;
-		    }
+		    if (n%3==0) n=n/3;
+			else if ((n-1)%3==0) n=n-1;
+			else if (n%2==0) n=n/2;
+			else n=n-1;
+			opertimes++;
 	    
 		}
 		System.out.println(opertimes);
