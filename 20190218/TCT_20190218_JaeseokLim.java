60b7f4004d2a1526bdb2923ebd0c5b61703b202d
task-jslim <nagaeatsuh@naver.com>
2019-02-18 15:57:23 +0900

    임재석commit

---
+++ 20190218/TCT_20190218_JaeseokLim.java
@@ -0,0 +1,46 @@
+﻿import java.util.Scanner;
+
+public class TCT_20190218_JaeseokLim {
+	static int zeroCnt = 0 ;
+	static int oneCnt = 0 ; 
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		int count = sc.nextInt();
+		
+		int[] pivonacci  = new int[count];
+		
+		for ( int i = 0 ; i < count; i++) {
+			pivonacci[i] = sc.nextInt();
+		}
+		
+		for ( int i = 0 ; i < count; i++) {
+			zeroCnt = 0 ;
+			oneCnt = 0 ;
+			calculatePiv(pivonacci[i]);
+			System.out.println(zeroCnt + " " + oneCnt );
+		}
+		
+    }
+	
+	private static int calculatePiv(int chk) {
+		if ( chk == 0 ) {
+			zeroCnt++;
+			//System.out.println(zeroCnt  + " " + oneCnt); 
+			return 0;
+		} else if ( chk == 1) {
+			oneCnt++;
+			//System.out.println(zeroCnt  + " " + oneCnt);
+			return 0;
+		} else {
+			return calculatePiv(chk-1) + calculatePiv(chk-2);
+		}
+		
+	}
+}
+		
