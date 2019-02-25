25fbeb08cef6e8a3eb6b145b35b28be39b3b211e
ultra1002 <ultra1002@naver.com>
2019-02-26 08:15:17 +0900

    박천희 commit

---
+++ 20190225/TCT_20190225_ParkChunHee.py
@@ -0,0 +1,30 @@
+maxMoney = 0
+
+def findMaxVal(inputArr):
+
+    global maxMoney
+    if len(inputArr) < 3:
+        maxMoney = max(inputArr)
+    else:
+        maxMoney+=max(inputArr)
+        maxIndex = inputArr.index(max(inputArr))
+        inputArr[maxIndex] = 0
+        if maxIndex == 0:
+            inputArr[maxIndex+1] = 0
+            inputArr[len(inputArr)-1] = 0
+        elif maxIndex == len(inputArr)-1:
+            inputArr[maxIndex-1] = 0
+            inputArr[0] = 0
+        else:
+            inputArr[maxIndex+1] = 0
+            inputArr[maxIndex-1] = 0
+
+    return maxMoney
+
+#inputArr = [2000, 3000]
+inputArr = [1000, 10000, 8000, 5000, 7000, 3000, 9000]
+
+while max(inputArr) > 0:
+    findMaxVal(inputArr)
+
+print(maxMoney)
