18371e87956d7e269f4c5e892c64e1ef89c0f364
easy-programmer <khr76693@lgcns.com>
2019-03-04 14:12:13 +0900

    김혜령 update

--- 20190304/TCT_20190304_HyeryoungKim.java
+++ 20190304/TCT_20190304_HyeryoungKim.java
@@ -7,42 +7,45 @@ public class TCT_20190304_HyeryoungKim {
     public static void main(String[] args) {
         TCT_20190304_HyeryoungKim tct = new TCT_20190304_HyeryoungKim();
         String input1 = "madam";
-        System.out.println(tct.getAdditionalAlphabetForPailndrome(input1));
+        System.out.println(tct.getAdditionalAlphabetForPalindrome(input1));
         
         String input2 = "lalavla";
-        System.out.println(tct.getAdditionalAlphabetForPailndrome(input2));
+        System.out.println(tct.getAdditionalAlphabetForPalindrome(input2));
         
         String input3 = "nurses run";
-        System.out.println(tct.getAdditionalAlphabetForPailndrome(input3));
+        System.out.println(tct.getAdditionalAlphabetForPalindrome(input3));
         
         String input4 = "level";
-        System.out.println(tct.getAdditionalAlphabetForPailndrome(input4));
+        System.out.println(tct.getAdditionalAlphabetForPalindrome(input4));
+        
+        String input5 = "abab";
+        System.out.println(tct.getAdditionalAlphabetForPalindrome(input5));
     }
     
-    private int getAdditionalAlphabetForPailndrome(String input) {
+    private int getAdditionalAlphabetForPalindrome(String input) {
         list = new ArrayList<String>();
         String str = input.replaceAll("\\p{Z}", "");
-        if ( !isPailndrome(str) ) {
-            makePailndrome(str, str.length()-1);
+        if ( !isPalindrome(str) ) {
+            makePalindrome(str, str.length()-2);
         }
         
         return list.size();
     }
     
-    private void makePailndrome(String str, int lastIndex) {
+    private void makePalindrome(String str, int lastIndex) {
         String tmp = str;
-        if ( !isPailndrome(str) ) {
+        if ( !isPalindrome(str) ) {
             char c = tmp.charAt(lastIndex);
             tmp = tmp + String.valueOf(c);
             if ( !list.contains(String.valueOf(c)) ) {
                 list.add(String.valueOf(c));
             }
             
-            makePailndrome(tmp, lastIndex-1);
+            makePalindrome(tmp, lastIndex-1);
         }
     }
     
-    private boolean isPailndrome(String input) {
+    private boolean isPalindrome(String input) {
         String strReverse = new StringBuilder(input).reverse().toString();
         if ( input.equals(strReverse) ) {
             return true;
