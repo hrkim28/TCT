387346c2a3eadbcdf1c1a059256a152d3c889c5e
spica07 <spica07@gmail.com>
2019-03-06 22:26:03 +0900

    이진경 commit

---
+++ 20190306/TCT_20190306_LeeJinKyoung.java
@@ -0,0 +1,39 @@
+public class TCT_20190306_LeeJinKyoung {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+        //String input = "long_and_mnemonic_identifier";
+        String input = "jobLog";
+        String result = "";
+        
+        if(!input.equals(input.toUpperCase()) && input.indexOf('_') > 0 ){
+            result = "Error!!!";
+        } else if(input.indexOf('_') > 0) {
+            for(int inx = 0; inx < input.length(); inx++){
+                if(input.charAt(inx) != '_'){
+                    result += input.charAt(inx);
+                } else {
+                    if(inx != input.length() -1){
+                        inx++;
+                        result += String.valueOf(input.charAt(inx)).toUpperCase();
+                    }
+                }
+            }
+        } else {
+            for(int inx = 0; inx < input.length(); inx++){
+                if((int)input.charAt(inx) < (int)'a'){
+                    result += '_';
+                    result += String.valueOf(input.charAt(inx)).toLowerCase();
+                } else {
+                    result += input.charAt(inx); 
+                }
+            }
+        }
+        
+        System.out.println(result);
+    }
+
+}
