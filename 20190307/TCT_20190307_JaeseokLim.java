10f3772b73ab0b349d8a94e88b540e8830f88aa5
task-jslim <nagaeatsuh@naver.com>
2019-03-07 16:30:30 +0900

    임재석 commit

---
+++ 20190307/TCT_20190307_JaeseokLim.java
@@ -0,0 +1,57 @@
+﻿import java.util.Arrays;
+import java.util.Scanner;
+
+
+
+public class TCT_20190307_JaeseokLim {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		int namuCnt = sc.nextInt();
+		int wantMeter = sc.nextInt();
+		int[] namu = new int[namuCnt];
+		for ( int i = 0 ; i < namuCnt; i++) {
+			namu[i] = sc.nextInt();
+		}
+		
+		System.out.println(fnMinMeter(namu,wantMeter));
+	
+		
+		
+    }
+	
+	
+	private static int fnMinMeter(int[] namu,int wantMeter) {
+		
+		int retVal = 0;
+		int tempM = 0 ;
+		
+		Arrays.sort(namu);
+		retVal = namu[namu.length-1]; 
+		
+		while (true) {
+			for ( int i =0 ; i< namu.length; i ++) {
+				if ( namu[i]> retVal) {
+					tempM += namu[i] - retVal;
+				}
+			}
+			if ( tempM >= wantMeter ) {
+				break;
+			} else {
+				retVal --;
+				tempM = 0;
+			}
+		}
+		
+		
+		
+		
+		return retVal;
+		
+	}
+
+}
+	
