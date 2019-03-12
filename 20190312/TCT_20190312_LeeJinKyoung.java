2b73a5f7c672ba1a90f275fea5efe41c4a5554ba
spica07 <spica07@gmail.com>
2019-03-12 17:13:34 +0900

    이진경 commit

---
+++ 20190312/TCT_20190312_LeeJinKyoung.java
@@ -0,0 +1,30 @@
+
+public class TCT_20190312_LeeJinKyoung {
+
+    public static void main(String[] args) {
+        int[][] result = makeCombination(4, 4);
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+
+    private static int[][] makeCombination(int N, int K) {
+        int arraySize = (int)Math.pow(N,N);
+        int[][] result = new int[arraySize][K];        
+        int num = 0;
+        
+        for(int i = 0 ; i < arraySize;i++){
+            num = i;
+            for(int j = K-1 ; j > 0; j--){                
+                result[i][(K-1) - j] = num / (int)Math.pow(K,j);
+                num -= result[i][(K-1) - j] * (int)Math.pow(K,j);
+            }
+            result[i][K-1] = num;
+        }
+
+        return result;
+    }
+}
