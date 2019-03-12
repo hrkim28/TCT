3b62d4f5396380b71258d679872c5267e52d05d5
ckh80 <46431692+ckh80@users.noreply.github.com>
2019-03-12 17:44:47 +0900

    최기훈 commit

--- 20190312/TCT_20190312_ChoiKiHun.py
+++ 20190312/TCT_20190312_ChoiKiHun.py
@@ -3,6 +3,9 @@ combination = []
 
 def main() :
 	makeCombination(4,4)
+	for a in combination[0] :
+		for b in combination[1] :
+			print(a+b)
 
 def makeCombination(n,k) :
 	for i in range(0,n) :
@@ -10,9 +13,5 @@ def makeCombination(n,k) :
 			combi1.append(str(i)+str(j))
 	combination.append(combi1)
 	combination.append(combi1)
-	
-	for a in combination[0] :
-		for b in combination[1] :
-			print(a+b)
 
 main()
