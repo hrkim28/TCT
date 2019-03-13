14e9df03ce283bbe451c5c5c2cfe6b58bde863a8
spica07 <spica07@gmail.com>
2019-03-13 14:16:39 +0900

    이진경 commit

---
+++ 20190313/TCT_20190313_LeeJinKyoung.java
@@ -0,0 +1,38 @@
+
+public class TCT_20190313_LeeJinKyoung {
+    public static void main(String[] args) {
+        int N = 4;
+        int K = 11;
+        
+        int[] input = {427,541,774,822};
+        
+        int inputSum = 0; 
+                
+        for(int inx = 0 ; inx < input.length; inx++){
+            inputSum = inputSum + input[inx];
+               
+        }
+        
+        int avg = inputSum/K;
+        int remain = 0;
+        int copyK = 3;
+        
+        for(int inx = avg ; inx > 0; inx--){
+            copyK = K;
+            
+            for(int jnx = 0 ; jnx < input.length; jnx++){
+                remain = input[jnx];
+                while(remain >= inx){
+                    remain -= inx;
+                    copyK--;
+                }
+            }
+            
+            if(copyK == 0){
+                System.out.println(inx);
+                break;
+            }
+            
+        }
+    }
+}
