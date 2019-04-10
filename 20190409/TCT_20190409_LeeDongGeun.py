059fdccc134cc55e81d5f6f701c0ee9ac829ad89
yelgit <yeloxen@naver.com>
2019-04-11 00:06:04 +0900

    수정

--- 20190409/TCT_20190409_LeeDongGeun.py
+++ 20190409/TCT_20190409_LeeDongGeun.py
@@ -4,26 +4,14 @@
 def compPrime(inputData):
     intCheck = 0
     resultVal = []
-    minCnt = 0
-    subCnt = 0
 
-    for i in range(1, inputData):
+    for i in range(1, inputData//2+1):
         intCheck = inputData - i
-        if checkPrime(i) == False or checkPrime(intCheck) == False:
-            # 연속수와 차감수가 동일한 경우 이후는 앞 비교와 동일하므로 반복문 중단함
-            if i == intCheck:
-                break
-            else:
-                continue
-        else:
-            if i == intCheck:
-                resultVal.append((i, intCheck))
-                break
-            else:
-                resultVal.append((i, intCheck))
+        if checkPrime(i) == True and checkPrime(intCheck) == True:
+            resultVal.append((i, intCheck))
 
     #연속수로 처리되므로 가장 마지막 처리된 값이 상호 차이가 가장 작음
-    return resultVal[-1]
+    print(resultVal[-1])
 
 def checkPrime(inputData):
     for i in range(2,inputData):
@@ -31,6 +19,6 @@ def checkPrime(inputData):
             return False
     return True
 
-inputData = 32
-a = compPrime(inputData)
-print(a)
+compPrime(6)
+compPrime(16)
+compPrime(32)
